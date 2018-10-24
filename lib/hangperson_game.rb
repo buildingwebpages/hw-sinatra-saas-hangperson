3b01b43e4bc3d2c9a04846b19df435f925fc0e98
Patrick Gardocki <pfg164@yahoo.com>
2018-10-24 01:01:30 +0000

    implement guessing letters, displaying word, and game win/lose status

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -5,11 +5,39 @@ class HangpersonGame
 
   # Get a word from remote "random word" service
 
-  # def initialize()
-  # end
+  attr_accessor :word, :guesses, :wrong_guesses
   
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+  
+  def check_valid(letter)
+     letter =~ /[a-zA-Z]/
+  end
+  
+  def guess(letter)
+    raise ArgumentError.new("Only upper/lower case letters allowed") unless check_valid(letter)
+    letter = letter.downcase
+
+    if @word.include?(letter)
+      return false if @guesses.include?(letter)
+      @guesses += letter
+    else
+      return false if @wrong_guesses.include?(letter)
+      @wrong_guesses += letter
+    end
+  end
+  
+  def word_with_guesses
+    @word.gsub(/[^"#{@guesses}"]/, '-')
+  end
+  
+  def check_win_or_lose
+    return :win if @word.split('').sort.join == @guesses.split('').sort.join
+    return :lose if @wrong_guesses.length == 7
+    return :play
   end
 
   # You can test it by running $ bundle exec irb -I. -r app.rb
